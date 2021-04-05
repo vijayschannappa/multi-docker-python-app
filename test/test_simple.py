25c5d180bcd4cbe8f94bcb86c760e104041d3645
Vijay <vijay.s@reconnectenergy.com>
2021-04-05 14:57:23 +0530

    added travis yml file

--- test/test_simple.py
+++ test/test_simple.py
@@ -1,12 +1,11 @@
 import unittest
 
-
 class CheckUnitTesting(unittest.TestCase):
-    def simple_test(self):
+    def test_simple(self):
         self.assertEqual(3,sum([1,2]))
 
-    def simple_test_2(self):
-        self.assertEqual(4,sum([1,4]))
+    def test_simple_2(self):
+        self.assertEqual(4,sum([1,3]))
 
 if __name__ == '__main__':
     unittest.main()
